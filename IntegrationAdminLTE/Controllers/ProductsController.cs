c80c721b151f8d6b5d414d24d94526c947c06cbe
Raouf Yasin <babaraoufyasin111@hotmail.com>
2021-06-11 22:25:52 +0100

    Add project files.

---
+++ IntegrationAdminLTE/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using IntegrationAdminLTE.Data;
+using IntegrationAdminLTE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationAdminLTE.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public StoreContext _context { get; }
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> ProductList()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            _context.Products.Add(product);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Create");
+        }
+    }
+}
---
+++ IntegrationAdminLTE/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using IntegrationAdminLTE.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationAdminLTE.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
